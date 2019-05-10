e6dc8e511dff495597328e0cd83ebd675a899103
sirin <sirin_md@hotmail.com>
2019-05-10 14:24:01 +0700

    c

--- wheel.js
+++ wheel.js
@@ -2,44 +2,44 @@
 var allText = ["In symptomatic SND, pacemaker improves symptoms; not mortality",
                 "In a patient with symptomatic sinus bradycardia from guideline-directed medication, pacemaker is indicated.",
                 "In symptomatic SND, atrial-based pacing (not ventricular based or VVI) is recommended.",
-                "Symptomatic high-grade AV block + stable AV node blockers --> proceed with pacemaker without further observation."
+                "Symptomatic high-grade AV block + stable dose of AV node blocker --> proceed with pacemaker without further observation."
             ];
-var bg = ['green','green','green','yellow']
+var bg = ['MediumSpringGreen','MediumSpringGreen','MediumSpringGreen','LemonChiffon']
 
 //functions
 var tStart = 0;
 //tStart is a global variable = where the wheel is.
-function showTxtBlk(tInit,tEnd){
-    var listNo=0; var txtIndex;
-    var tMax=allText.length; 
-    var card1 = document.getElementsByClassName('card1');
-    card1[0].classList.remove('animateLeft10');
+function showTxtBlk(){
+    var card = document.getElementById('card');
+    card.innerHTML = allText[tStart]   
+    card.style.backgroundColor = bg[tStart]
+    var num = Math.floor(Math.random() * 10); 
+    var mark = Math.floor(Math.random()*2);var deg;
+    if (mark==1){deg=(num/3)*-1}
+    else {deg=num/3}
+    card.style.transform = 'rotate('+deg+'deg)'
 
-    for (i=tInit; i<=tEnd; i++){
-      var txt = document.getElementsByClassName("card"+listNo);
-      if (i>=tMax){
-        txtIndex=i-tMax;
-      } else {txtIndex=i}
-            
-      card1[0].classList.add('animateLeft10');
-      txt[0].innerHTML = allText[txtIndex];
-            listNo++;
-      if (listNo>2) {listNo=0};
-    } 
+    //show nav
+    var nav = document.getElementById('bradyWheelCenter')
+    var navNo = tStart + 1;
+    nav.innerHTML = navNo + ' / ' + allText.length;
+    //arrow
+    var left = document.getElementById('bradyWheelLeft')
+    var right = document.getElementById('bradyWheelRight')
+    left.onclick = function(){rw()}
+    right.onclick = function(){fwd()}
 }
 
 function fwd() {
     ++tStart;
     if (tStart>=allText.length){tStart=0}
-    var tn=tStart+2;
-    showTxtBlk(tStart,tn);
+    showTxtBlk();
       }
     
 function rw() {
     --tStart;
     if (tStart<0){tStart=(allText.length)-1}
-    var tn=tStart+2;
-    showTxtBlk(tStart,tn);
+    showTxtBlk();
     }   
 
 var init;
@@ -54,9 +54,8 @@ function autoFwd(){
     init = setInterval(function(){
     ++tStart;
     if (tStart>=allText.length){tStart=0}
-    var tn=tStart+2;
-    showTxtBlk(tStart,tn);
-    } , 1000);}
+      showTxtBlk();
+    } , 4400);}
     
 }
     
